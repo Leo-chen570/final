f1b9c866008e758c5fdce142ea6539b83e54996d
Leo-chen570 <493075073@qq.com>
2020-01-07 00:15:16 -0800

    added a EX1.c file

---
+++ EX1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+    double x;
+    double y;
+    double z;
+    printf("please input two numbers(divided by space)\n");
+    scanf("%lf %lf",&x,&y);
+    z=(x+y)*(x+y);
+    printf("%.2lf\n",z);
+}
